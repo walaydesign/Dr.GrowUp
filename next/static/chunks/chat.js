24b9ec16c00efa847620d871e5c3b19f42ad8124
youzhen <yl301114@gmail.com>
2026-03-16 16:13:21 +0000

    改資料夾名

--- next/static/chunks/chat.js
+++ next/static/chunks/chat.js
@@ -1,3 +1,20 @@
+window.addEventListener("load", () => {
+  gsap.registerPlugin(TextPlugin, SplitText);
+  //   初始頁面動畫
+  let split = SplitText.create(".init-text", {
+    type: "lines",
+    linesClass: "line",
+  });
+
+  gsap.from(split.lines, {
+    yPercent: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: "power2.out",
+  });
+});
+
 $(document).ready(function () {
   $(".chat_input_send").click(function () {
     if ($(".chat-input-main").val() !== "") {
@@ -16,7 +33,7 @@ $(document).ready(function () {
       let typing = `<div class='typing'><div class='dot dot-1'></div><div class='dot dot-2'></div><div class='dot dot-3'></div></div>`;
       let typingAnswer = $("<div class='answer_text'></div>").html(typing);
       let avatar = $(
-        "<div class='answer_logo'><img src='./_next/logo.png' /><p>剛剛好 AI</p></div>",
+        "<div class='answer_logo'><img src='./next/logo.png' /><p>剛剛好 AI</p></div>",
       );
       let answer = $("<div class='answer'></div>");
       answer.append(avatar, typingAnswer);
@@ -37,33 +54,29 @@ $(document).ready(function () {
           let answerTextWrap = $("<p class='answer_text'></p>").html(
             answerText,
           );
+            // let answerTextWrap = $("<p class='answer_text'>地今杯穴己上亭古發紅太間勿青、造且美實在裏生教。都六品條，水壯具放牠田王真兆怪路四現，法打男抓能拍！別各午在步課花又力！再手亮化冰貝吧。貓犬彩。尺彩現三們貓尾喝爸就點坐地洋何口爬急東免「起收共躲飽」動鳥圓定是穴鳥乞走上紅「是這她者他綠牙多中連」忍兩。音泉帽士衣蛋點相去正？犬們小走！長定爬真畫太姐見實石又現工巴寸頭比，寸對乙申學後抱林苦至樹教尺同，追乙現我結笑干雲他。種世化司日眼祖請邊知松長完麻斤多七草車就，耍交借爸實明門找半「皮千跑公兆服」。</p>");
 
-          let answerImgUrl = "./_next/pic.jpg";
-            let answerImg = $("<img>").attr("src", answerImgUrl);
-            let answerImgWrap = $("<div class='answer_pic'></div>");
-            answerImgWrap.append(answerImg);
+          let answerImgUrl = "./next/pic.jpg";
+          let answerImg = $("<img>").attr("src", answerImgUrl);
+          let answerImgWrap = $("<div class='answer_pic'></div>");
+          answerImgWrap.append(answerImg);
           answer.empty();
           answer.append(avatar, answerTextWrap, answerImgWrap);
           $(".chat_wrapper.chatting").append(answer);
+
+          answer.append(avatar, answerTextWrap, answerImgWrap);
+
+          // GSAP 打字動畫
+          gsap.to(answerTextWrap[0], {
+            duration: 3,
+            text: {
+              value: answerText,
+              delimiter: "",
+            },
+            ease: "none",
+          });
         }, 1000);
       }, 500);
     }
-  });
-});
-
-window.addEventListener("load", () => {
-  gsap.registerPlugin(SplitText);
-  //   初始頁面動畫
-  let split = SplitText.create(".init-text", {
-    type: "lines",
-    linesClass: "line",
-  });
-
-  gsap.from(split.lines, {
-    yPercent: 100,
-    opacity: 0,
-    duration: 1,
-    stagger: 0.2,
-    ease: "power2.out",
   });
 });
